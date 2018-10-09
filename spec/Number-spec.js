9c732201a3021641f3ea21e95ad6d882743e0d52
Brian Nelson and Victoria Oh <victoria@>
2018-10-09 11:26:48 -0700

    add tests for roman numeral function

--- spec/Number-spec.js
+++ spec/Number-spec.js
@@ -2,11 +2,32 @@ import { ConvertToRomanNumeral } from './../src/Number.js';
 
 describe('ConvertToRomanNumeral', function() {
 
-    it('should test whether it returns the same number as input', function() {
 
-      expect(ConvertToRomanNumeral(1)).toEqual(1);
 
-    });
+    it('should test whether it returns corresponding first roman numerals1', function() {
+        var number = ConvertToRomanNumeral(5);
+        expect(number).toEqual("V");
+      });
+
+      it('should test whether it returns corresponding first roman numerals2', function() {
+        var number = ConvertToRomanNumeral(1);
+        expect(number).toEqual("I");
+      });
+
+      it('should test whether it returns corresponding first roman numerals3', function() {
+        var number = ConvertToRomanNumeral(100);
+        expect(number).toEqual("C");
+      });
+
+      it('should test whether it returns the entire roman numeral', function() {
+        var number = ConvertToRomanNumeral(3);
+        expect(number).toEqual("III");
+      });    
+      
+      it('should test whether it returns a number too big for a roman numeral', function() {
+        var number = ConvertToRomanNumeral(4011);
+        expect(number).toEqual("Number too big.");
+      });         
   
   });
 
--- src/Number.js
+++ src/Number.js
@@ -1,6 +1,22 @@
-// var decimalValue = [1,4,5,9,10,40,50,90,100,400,500,900,1000];
-// var romanNumeral = ["I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M"];
+var decimalValue = [1,4,5,9,10,40,50,90,100,400,500,900,1000];
+var romanNumeral = ["I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M"];
 
-export function ConvertToRomanNumeral(number) {
-    return number;
+export function ConvertToRomanNumeral (number) {
+    var outputVar = '';
+    var remainder;
+    if (number >= 4000) {
+      return "Number too big.";
+    } else {
+      for(;remainder !== 0;) {
+        for (var i= romanNumeral.length; i >= 0; i--){
+          if (number >= decimalValue[i]){
+            outputVar = outputVar + romanNumeral[i];
+            remainder = number - decimalValue[i];
+            i = 0;
+          }
+        }
+        number = remainder;
+      }
+    }
+    return outputVar;
 }
